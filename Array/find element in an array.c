a20b8dae5b8f08e92e6f4504582b3fdc26a48ae7
Prachi-2507 <166148780+Prachi-2507@users.noreply.github.com>
2025-10-21 10:59:54 +0530

    Create find element in an array.c

---
+++ Array/find element in an array.c
@@ -0,0 +1,9 @@
+// User function Template for C
+int search(int arr[], int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (x == arr[i]) {
+            return i;
+        }
+    }
+    return -1;
+}
