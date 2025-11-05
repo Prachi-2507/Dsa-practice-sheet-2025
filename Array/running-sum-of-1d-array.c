792cb8889c59296d6fb5507ab99e11f952ad5ade
Prachi-2507 <166148780+Prachi-2507@users.noreply.github.com>
2025-11-05 17:02:10 +0530

    Update running-sum-of-1d-array.c++

--- Array/running-sum-of-1d-array.c
+++ Array/running-sum-of-1d-array.c
@@ -1,12 +1,10 @@
-
-int* runningSum(int* nums, int numsSize, int* returnSize) {
-    int* runningSum = (int*)malloc(numsSize * sizeof(int));  
-    int sum = 0;
-
-    for (int i = 0; i < numsSize; i++) {
-        sum = sum + nums[i];              
-        runningSum[i] = sum;  }
-
-    *returnSize = numsSize;          
-    return runningSum;
-}
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 1; i < n; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+};
